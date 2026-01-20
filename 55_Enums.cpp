250be08b0bf3bdd364468f9e03f3a5ebf6c36301
Benjamin <jh0420park@gmail.com>
2026-01-20 16:49:17 +0900

    fffs

---
+++ 55_Enums.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+
+
+int main() {
+
+    // enums = a user-defined data type that consists
+    //         of paired named-integer constants.
+    //         GREAT if you have a set of potential options
+
+
+    std::string today = "sunday";
+
+    // switch(today) {
+    //     case "sunday":  std::cout << "It is Sunday!\n";
+    //                     break;
+    //     case "monday":  std::cout << "It is Monday!\n";
+    //                     break;
+    //     case "tuesday":  std::cout << "It is Tuesday!\n";
+    //                     break;
+    //     case "wednesday":  std::cout << "It is Wednesday!\n";
+    //                     break;
+    //     case "thursday":  std::cout << "It is Thursday!\n";
+    //                     break;
+    //     case "friday":  std::cout << "It is Friday!\n";
+    //                     break;
+    //     case "saturday":  std::cout << "It is Saturday!\n";
+    //                     break;
+    // }
+
+    // This wouldn't work because switch feature only works for integers
+
+    return 0;
+}
